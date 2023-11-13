6bba08e27b95fb832d36276fc7d3363c8c6b1e39
SickHr <blawatjustin@gmail.com>
2023-11-13 19:54:55 +0100

    v 0.1

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import time
 
-
 def shutdown(timer):
-    minutes = timer * 60
+    minutes = timer
 
-    time.sleep(minutes)
+    for remaining_time in range(minutes, 0, -1):
+        print(f"Remaining time till shutdown: {remaining_time} minutes")
+        time.sleep(60)  # hier wird 60 Sekunden gewartet, da wir in Minuten zählen
 
+    print("Shutting down...")
     os.system("shutdown -s")
 
-
-shutdown(1)
+shutdown(60)  # Hier kannst du die gewünschte Zeit in Minuten ändern
